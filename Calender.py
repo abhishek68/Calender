3dbb4b590ef0bbc28e8754ce49b8b19edc87e3fc
abhishek68 <75883645+abhishek68@users.noreply.github.com>
2021-03-21 10:28:08 +0530

    Add files via upload

---
+++ Calender.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 19:39:42 2021
+
+@author: ABHISHEK
+"""
+import calendar
+Year=int(input("Enter your year"))
+Month=int(input("Enter your month"))
+print(calendar.month(Year,Month))
